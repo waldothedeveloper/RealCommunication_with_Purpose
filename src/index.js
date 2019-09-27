efbc73c5aeb18f6a5454659ab5ff4d6eaa7675ee
Waldo Lavaut <waldothedeveloper@gmail.com>
2019-09-26 21:33:31 -0400

    Added Sentry to track bugs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import * as Sentry from "@sentry/browser";
 import * as serviceWorker from "./serviceWorker";
+
+Sentry.init({
+  dsn: "https://e92c6b343b004daeb9a21f88faa0ab17@sentry.io/1761838"
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
